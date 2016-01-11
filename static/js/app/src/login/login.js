3698cf195044a1987d9b958c40516578783a4bcc
huwanqi <hujiaojiao@eluying.net>
2016-01-11 14:26:00 +0800

    解决手机登首页问题

--- static/js/app/src/login/login.js
+++ static/js/app/src/login/login.js
@@ -118,21 +118,26 @@ $(document).ready(function(){
         show: false
     });
 
-    //function centerModals(){
-    //    $('.modal').each(function(){
-    //        var $clone = $(this).clone().css('display', 'block').appendTo('body');
-    //        var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2) - 52;
-    //        top = top > 0 ? top : 0;
-    //        $clone.remove();
-    //        $(this).find('.modal-content').css("margin-top", top);
-    //    });
-    //}
-    //$('.modal').on('show.bs.modal', centerModals);
-    //$(window).on('resize', centerModals);
-    //$(".btn-cancel").click(function(){
-    //    $(this).parents(".modal").modal("hide");
-    //});
-    //modal.modalInit();
+    if($(window).width() < 1200){
+        $("#loginBox").css({
+            "overflow-x": "hidden"
+        });
+    }else{
+        $("#loginBox").css({
+            "overflow-x": ""
+        });
+    }
+    $("body").on("resize", function(){
+        if($(window).width() < 1200){
+            $("#loginBox").css({
+                "overflow-x": "hidden"
+            });
+        }else{
+            $("#loginBox").css({
+                "overflow-x": ""
+            });
+        }
+    });
 
     $("#loginName").on("blur", GetPwdAndChk);
     //$("#loginSave").on("click", SetPwdAndChk);
