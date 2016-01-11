2715e5c50968706d24ff47c68c4391eed994612e
lingchenxuan <lingchenxuan@eluying.com>
2016-01-11 13:53:07 +0800

    md5

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp'),
-	sass = require('gulp-ruby-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	rename = require('gulp-rename'),
-	notify = require('gulp-notify'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
     webpack = require("webpack"),
     gulpWebpack = require("webpack-stream"),
     path = require("path"),
@@ -11,28 +11,36 @@ var gulp = require('gulp'),
     reload = browserSync.reload,
     zip = require('gulp-zip'),
     uglify = require('gulp-uglify'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    rev = require('gulp-rev'),
+    revCollector = require('gulp-rev-collector');
 
-gulp.task('browser-sync', function() {
-	browserSync.init({
-		server: {
-			baseDir: "./",
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./",
             index: "login.html"
-		}
-	});
+        }
+    });
 });
 
-gulp.task('clean', function(){
-   gulp.src(['./build', './.sass-cache'])
-       .pipe(clean({force: true}));
+gulp.task('clean', function () {
+    gulp.src(['./build', './.sass-cache'])
+        .pipe(clean({force: true}));
 });
 
-gulp.task('build', ['styles', 'webpack2', 'clean'],function(){
+gulp.task('build', ['styles', 'webpack2', 'clean'], function () {
     gulp.src('static/css/**/*.css')
-        .pipe(gulp.dest('build/static/css'));
+        .pipe(rev())
+        .pipe(gulp.dest('build/static/css'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('rev/css'));
     gulp.src('static/js/app/dist/**/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('build/static/js/app/dist'));
+        .pipe(rev())
+        .pipe(gulp.dest('build/static/js/app/dist'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('rev/js'));
     gulp.src(['static/js/lib/html5shiv.min.js', 'static/js/lib/respond.min.js', 'static/js/lib/bootstrap-ie.js'])
         .pipe(uglify())
         .pipe(gulp.dest('build/static/js/lib'));
@@ -43,26 +51,35 @@ gulp.task('build', ['styles', 'webpack2', 'clean'],function(){
     gulp.src('WEB-INF/web.xml')
         .pipe(gulp.dest('build/WEB-INF'));
     gulp.src('static/image/**/*')
-        .pipe(gulp.dest('build/static/image'));
-
+        .pipe(rev())
+        .pipe(gulp.dest('build/static/image'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('rev/image'));
 });
 
-gulp.task('zip', function(){
+gulp.task('zip', function () {
     gulp.src('./build/**')
         .pipe(zip('eluyun.war'))
         .pipe(gulp.dest('./'));
 });
 
-gulp.task('styles',function(){
-	return sass('static/sass/main.scss', {style: 'compressed'})
-		.pipe(autoprefixer('last 3 version'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('static/css'))
-		.pipe(notify({title:'好棒啊！',message:'css编译完成，站起来活动活动'}))
+gulp.task('rev', function () {
+    return gulp.src(['rev/**/*.json', 'build/**/*.html'])
+        .pipe( revCollector())
+        .pipe( gulp.dest('build') );
+});
+
+
+gulp.task('styles', function () {
+    return sass('static/sass/main.scss', {style: 'compressed'})
+        .pipe(autoprefixer('last 3 version'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('static/css'))
+        .pipe(notify({title: '好棒啊！', message: 'css编译完成，站起来活动活动'}))
         .pipe(reload({stream: true}));
 });
 
-gulp.task('webpack2', function(){
+gulp.task('webpack2', function () {
     return gulp.src('static/js/app/src/entry.js')
         .pipe(gulpWebpack({
             plugins: [
@@ -91,41 +108,40 @@ gulp.task('webpack2', function(){
             resolve: {
                 extensions: ['.js', ""],
                 alias: {
-                    cookie: path.join(__dirname,"./static/js/lib/jquery.cookie.js"),
-                    jquery: path.join(__dirname,"./static/js/lib/jquery.min.js"),
-                    jqueryui: path.join(__dirname,"./static/js/lib/jquery-ui.min.js"),
-                    "datepicker-zh": path.join(__dirname,"./static/js/lib/datepicker-zh-CN.js"),
-                    bootstrap: path.join(__dirname,"./static/js/lib/bootstrap.min.js"),
-                    header: path.join(__dirname,"./static/js/app/src/common/header.js"),
-                    leftMenu: path.join(__dirname,"./static/js/app/src/common/leftMenu.js"),
-                    util: path.join(__dirname,"./static/js/app/src/common/util.js"),
-                    logout: path.join(__dirname,"./static/js/app/src/common/logout.js"),
-                    AJAXService: path.join(__dirname,"./static/js/app/src/common/AJAXService.js"),
-                    accommodationPriceList: path.join(__dirname,"./static/js/app/src/price/accommodationPriceList.js"),
-                    foodETPriceList: path.join(__dirname,"./static/js/app/src/price/foodETPriceList.js"),
-                    trToggle: path.join(__dirname,"./static/js/app/src/common/trToggle.js"),
-                    loginValidate: path.join(__dirname,"./static/js/app/src/login/login.validate.js"),
-                    modal: path.join(__dirname,"./static/js/app/src/common/modal.js"),
-                    validate: path.join(__dirname,"./static/js/lib/jquery.validate.min.js"),
-                    validation: path.join(__dirname,"./static/js/lib/validation.js"),
+                    cookie: path.join(__dirname, "./static/js/lib/jquery.cookie.js"),
+                    jquery: path.join(__dirname, "./static/js/lib/jquery.min.js"),
+                    jqueryui: path.join(__dirname, "./static/js/lib/jquery-ui.min.js"),
+                    "datepicker-zh": path.join(__dirname, "./static/js/lib/datepicker-zh-CN.js"),
+                    bootstrap: path.join(__dirname, "./static/js/lib/bootstrap.min.js"),
+                    header: path.join(__dirname, "./static/js/app/src/common/header.js"),
+                    leftMenu: path.join(__dirname, "./static/js/app/src/common/leftMenu.js"),
+                    util: path.join(__dirname, "./static/js/app/src/common/util.js"),
+                    logout: path.join(__dirname, "./static/js/app/src/common/logout.js"),
+                    AJAXService: path.join(__dirname, "./static/js/app/src/common/AJAXService.js"),
+                    accommodationPriceList: path.join(__dirname, "./static/js/app/src/price/accommodationPriceList.js"),
+                    foodETPriceList: path.join(__dirname, "./static/js/app/src/price/foodETPriceList.js"),
+                    trToggle: path.join(__dirname, "./static/js/app/src/common/trToggle.js"),
+                    loginValidate: path.join(__dirname, "./static/js/app/src/login/login.validate.js"),
+                    modal: path.join(__dirname, "./static/js/app/src/common/modal.js"),
+                    validate: path.join(__dirname, "./static/js/lib/jquery.validate.min.js"),
+                    validation: path.join(__dirname, "./static/js/lib/validation.js"),
                     seasonManage: path.join(__dirname, "./static/js/app/src/price/seasonManage.js"),
                     monthManage: path.join(__dirname, "./static/js/app/src/price/monthManage.js"),
                     floatInfo: path.join(__dirname, "./static/js/app/src/common/floatInfo.js"),
                     roomCategoryList: path.join(__dirname, "./static/js/app/src/category/room/roomCategoryList.js"),
-                    fileupload: path.join(__dirname,"./static/js/lib/jquery.fileupload.js"),
-                    "jquery.ui.widget": path.join(__dirname,"./static/js/lib/jquery.ui.widget.js"),
-                    iframe: path.join(__dirname,"./static/js/lib/jquery.iframe-transport.js")
+                    fileupload: path.join(__dirname, "./static/js/lib/jquery.fileupload.js"),
+                    "jquery.ui.widget": path.join(__dirname, "./static/js/lib/jquery.ui.widget.js"),
+                    iframe: path.join(__dirname, "./static/js/lib/jquery.iframe-transport.js")
                 }
             },
-        }, null, function(err, stats) {
-            if(err) throw new gutil.PluginError("webpack", err);
-            gutil.log("[webpack]", stats.toString({
-            }));
+        }, null, function (err, stats) {
+            if (err) throw new gutil.PluginError("webpack", err);
+            gutil.log("[webpack]", stats.toString({}));
         }, webpack))
         .pipe(gulp.dest('static/js/app/dist/'));
 });
 
-gulp.task('webpack', function(){
+gulp.task('webpack', function () {
     return gulp.src('static/js/app/src/entry.js')
         .pipe(gulpWebpack({
             plugins: [
@@ -154,55 +170,53 @@ gulp.task('webpack', function(){
             resolve: {
                 extensions: ['.js', ""],
                 alias: {
-                    cookie: path.join(__dirname,"./static/js/lib/jquery.cookie.js"),
-                    jquery: path.join(__dirname,"./static/js/lib/jquery.min.js"),
-                    jqueryui: path.join(__dirname,"./static/js/lib/jquery-ui.min.js"),
-                    "datepicker-zh": path.join(__dirname,"./static/js/lib/datepicker-zh-CN.js"),
-                    bootstrap: path.join(__dirname,"./static/js/lib/bootstrap.min.js"),
-                    header: path.join(__dirname,"./static/js/app/src/common/header.js"),
-                    leftMenu: path.join(__dirname,"./static/js/app/src/common/leftMenu.js"),
-                    util: path.join(__dirname,"./static/js/app/src/common/util.js"),
-                    logout: path.join(__dirname,"./static/js/app/src/common/logout.js"),
-                    AJAXService: path.join(__dirname,"./static/js/app/src/common/AJAXService.js"),
-                    accommodationPriceList: path.join(__dirname,"./static/js/app/src/price/accommodationPriceList.js"),
-                    foodETPriceList: path.join(__dirname,"./static/js/app/src/price/foodETPriceList.js"),
-                    trToggle: path.join(__dirname,"./static/js/app/src/common/trToggle.js"),
-                    loginValidate: path.join(__dirname,"./static/js/app/src/login/login.validate.js"),
-                    modal: path.join(__dirname,"./static/js/app/src/common/modal.js"),
-                    validate: path.join(__dirname,"./static/js/lib/jquery.validate.min.js"),
-                    validation: path.join(__dirname,"./static/js/lib/validation.js"),
+                    cookie: path.join(__dirname, "./static/js/lib/jquery.cookie.js"),
+                    jquery: path.join(__dirname, "./static/js/lib/jquery.min.js"),
+                    jqueryui: path.join(__dirname, "./static/js/lib/jquery-ui.min.js"),
+                    "datepicker-zh": path.join(__dirname, "./static/js/lib/datepicker-zh-CN.js"),
+                    bootstrap: path.join(__dirname, "./static/js/lib/bootstrap.min.js"),
+                    header: path.join(__dirname, "./static/js/app/src/common/header.js"),
+                    leftMenu: path.join(__dirname, "./static/js/app/src/common/leftMenu.js"),
+                    util: path.join(__dirname, "./static/js/app/src/common/util.js"),
+                    logout: path.join(__dirname, "./static/js/app/src/common/logout.js"),
+                    AJAXService: path.join(__dirname, "./static/js/app/src/common/AJAXService.js"),
+                    accommodationPriceList: path.join(__dirname, "./static/js/app/src/price/accommodationPriceList.js"),
+                    foodETPriceList: path.join(__dirname, "./static/js/app/src/price/foodETPriceList.js"),
+                    trToggle: path.join(__dirname, "./static/js/app/src/common/trToggle.js"),
+                    loginValidate: path.join(__dirname, "./static/js/app/src/login/login.validate.js"),
+                    modal: path.join(__dirname, "./static/js/app/src/common/modal.js"),
+                    validate: path.join(__dirname, "./static/js/lib/jquery.validate.min.js"),
+                    validation: path.join(__dirname, "./static/js/lib/validation.js"),
                     seasonManage: path.join(__dirname, "./static/js/app/src/price/seasonManage.js"),
                     monthManage: path.join(__dirname, "./static/js/app/src/price/monthManage.js"),
                     floatInfo: path.join(__dirname, "./static/js/app/src/common/floatInfo.js"),
                     roomCategoryList: path.join(__dirname, "./static/js/app/src/category/room/roomCategoryList.js"),
-                    fileupload: path.join(__dirname,"./static/js/lib/jquery.fileupload.js"),
-                    "jquery.ui.widget": path.join(__dirname,"./static/js/lib/jquery.ui.widget.js"),
-                    iframe: path.join(__dirname,"./static/js/lib/jquery.iframe-transport.js")
+                    fileupload: path.join(__dirname, "./static/js/lib/jquery.fileupload.js"),
+                    "jquery.ui.widget": path.join(__dirname, "./static/js/lib/jquery.ui.widget.js"),
+                    iframe: path.join(__dirname, "./static/js/lib/jquery.iframe-transport.js")
                 }
             },
-        devtool: "sourcemap"
-        }, null, function(err, stats) {
-            if(err) throw new gutil.PluginError("webpack", err);
-            gutil.log("[webpack]", stats.toString({
-            }));
+            devtool: "sourcemap"
+        }, null, function (err, stats) {
+            if (err) throw new gutil.PluginError("webpack", err);
+            gutil.log("[webpack]", stats.toString({}));
         }, webpack))
         .pipe(gulp.dest('static/js/app/dist/'))
         .pipe(reload({stream: true}))
-        .pipe(notify({title:'好棒啊！',message:'<%= file.relative %>编译完成，站起来活动活动'}));
+        .pipe(notify({title: '好棒啊！', message: '<%= file.relative %>编译完成，站起来活动活动'}));
 });
 
 
-
-gulp.task('watch',function(){
-	gulp.watch('static/sass/**/*.scss',['styles']);
-	gulp.watch('static/js/app/src/**/*.js',['webpack']);
-	gulp.watch("**/*.html").on('change', reload);
+gulp.task('watch', function () {
+    gulp.watch('static/sass/**/*.scss', ['styles']);
+    gulp.watch('static/js/app/src/**/*.js', ['webpack']);
+    gulp.watch("**/*.html").on('change', reload);
 });
 
-gulp.task('default',['watch'],function(){
-	gulp.start('browser-sync');
-	gulp.start('styles');
-	gulp.start('webpack');
-	gulp.start('watch');
+gulp.task('default', ['watch'], function () {
+    gulp.start('browser-sync');
+    gulp.start('styles');
+    gulp.start('webpack');
+    gulp.start('watch');
 });
 
