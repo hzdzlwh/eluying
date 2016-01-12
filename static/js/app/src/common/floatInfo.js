5206b5ac158502bd66cb49323cfe6cc448882cad
lingchenxuan <lingchenxuan@eluying.com>
2016-01-12 15:27:01 +0800

    悬浮窗换行

--- static/js/app/src/common/floatInfo.js
+++ static/js/app/src/common/floatInfo.js
@@ -9,7 +9,7 @@ var floatInfo = {
         document.addEventListener('mousemove', getPos);
         moreInfoT = setTimeout(function(){
             $("body").append("<div class='moreInfo'></div>");
-            $(".moreInfo").html($(that).html()).css("top", y).css("left", x);
+            $(".moreInfo").html($(that).html().replace(/\n|\r|(\r\n)|(\u0085)|(\u2028)|(\u2029)/g, "<br>")).css("top", y + 10).css("left", x + 10);
             document.removeEventListener('mousemove', getPos);
         }, 500);
     },
