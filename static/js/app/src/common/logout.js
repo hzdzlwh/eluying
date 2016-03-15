0d014156c0ed76864a5773b2c9eaf4ba080b3530
lingchenxuan <lingchenxuan@eluying.com>
2016-03-15 11:57:51 +0800

    fix: cookie清除问题

--- static/js/app/src/common/logout.js
+++ static/js/app/src/common/logout.js
@@ -1,15 +1,15 @@
 var AJAXService = require("AJAXService");
 var logout = {
-    logout: function(){
+    logout: function() {
         $.get(AJAXService.getUrl("logoutUrl"));
         localStorage.clear();
-        location.href = "/login.html";
-        var keys=document.cookie.match(/[^ =;]+(?=\=)/g); 
+        var keys = document.cookie.match(/[^ =;]+(?=\=)/g); 
 		if (keys) {
 			for (var i = keys.length; i--;) {
-				document.cookie=keys[i]+'=0;expires=' + new Date(0).toUTCString() 
+				document.cookie=keys[i]+'=0;expires=' + new Date(0).toUTCString(); 
 			}
-		} 
+		}
+		location.href = "/login.html";
     }
 };
 module.exports = logout;
