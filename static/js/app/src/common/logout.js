75f08424169c217fd3b00a348599bddb0d9244b7
lingchenxuan <lingchenxuan@eluying.com>
2016-03-15 11:44:32 +0800

    fix: 登出时没有清楚cookie

--- static/js/app/src/common/logout.js
+++ static/js/app/src/common/logout.js
@@ -4,6 +4,12 @@ var logout = {
         $.get(AJAXService.getUrl("logoutUrl"));
         localStorage.clear();
         location.href = "/login.html";
+        var keys=document.cookie.match(/[^ =;]+(?=\=)/g); 
+		if (keys) {
+			for (var i = keys.length; i--;) {
+				document.cookie=keys[i]+'=0;expires=' + new Date(0).toUTCString() 
+			}
+		} 
     }
 };
 module.exports = logout;
