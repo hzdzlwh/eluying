4f56f64b5d4bf950d08430193c854cca9c15df55
hwq <huwanqi1234@gmail.com>
2016-05-06 21:20:00 +0800

    HUWANQI: 房态日历基本样式

--- static/js/app/src/accommodation/calender/calender.js
+++ static/js/app/src/accommodation/calender/calender.js
@@ -16,24 +16,30 @@ require("validation");
 $(function(){
     //初始化界面
     header.showHeader();
-    leftMenu.showLeftMenu();
+    //高亮"前台录入"
+    $(".settingsEntry").removeClass("selected");
+    $(".accomodationEntry").addClass("selected");
     topMenu.showTopMenu();
-    util.mainContainer();
     modal.modalInit();
 
     //初始化日历
-    $.datepicker.setDefaults( $.datepicker.regional[ "zh-CN" ] );
-    $("#datePicker").datepicker({
-        dateFormat: "yy-mm-dd",
-        changeMonth: true,
-        changeYear: true
-    });
-    $("#datePicker").datepicker( "setDate", new Date());
+    // $.datepicker.setDefaults( $.datepicker.regional[ "zh-CN" ] );
+    // $("#datePicker").datepicker({
+    //     dateFormat: "yy-mm-dd",
+    //     changeMonth: true,
+    //     changeYear: true
+    // });
+    // $("#datePicker").datepicker( "setDate", new Date());
 
     events = {
 
-        "resize window": util.mainContainer,
         "show.bs.modal .modal": modal.centerModals,
+        "scroll .calendor-container": function(){
+            var scrollLeft = $(this).scrollLeft();
+            var scrollTop = $(this).scrollTop();
+            $(".accommodation-mainContainer .content .sheader").css("margin-left", -scrollLeft);
+            $(".accommodation-mainContainer .content .leftHeader").css("margin-top", -scrollTop);
+        },
         "click .btn-cancel": function(){var that = this; modal.clearModal(that);}
 
     };
