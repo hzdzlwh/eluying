da81c5f17e139f4bd26fea8b72e30908c4892da0
lingchenxuan <lingchenxuan@eluying.com>
2016-08-29 14:54:00 +0800

    fix: 弹框居中的bug

--- static/js/app/src/business/category/entertainment/ETEntry.js
+++ static/js/app/src/business/category/entertainment/ETEntry.js
@@ -35,12 +35,8 @@ $(function(){
 
 
     var events = {
-
         "resize window": util.mainContainer,
         "show.bs.modal .modal": modal.centerModals,
-        "click .btn-cancel": function(){var that = this; modal.clearModal(that);}
-
-
     };
 
     util.bindDomAction(events);
