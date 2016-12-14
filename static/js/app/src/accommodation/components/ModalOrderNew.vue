6aa68bd4391ba8b3c841a0bc2e48add2cfa26fad
赵永生 <zhaoyongsheng@eluying.com>
2016-12-14 17:07:08 +0800

    feat: add new feat of modal

---
+++ static/js/app/src/accommodation/components/ModalOrderNew.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+    </div>
+</template>
+<style lang="sass">
+    @import "~dd-common-css/src/variables";
+</style>
+<script>
+    export default{
+        data:{}
+    }
+</script>
