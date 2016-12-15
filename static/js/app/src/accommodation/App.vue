dc222f1bbf74e3d325680f23e326ea1cf77c20ce
lcx <258546853@qq.com>
2016-12-15 15:16:18 +0800

    feat(2.2）: 房态

--- static/js/app/src/accommodation/App.vue
+++ static/js/app/src/accommodation/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Calendar />
+        <Calendar :categories="categories" :dateRange="dateRange" :holidays="holidays" :roomStatus="roomStatus" />
     </div>
 </template>
 <style>
@@ -15,16 +15,55 @@
             Promise.all([
                 AJAXService.ajaxWithToken('get', '/room/getRoomCategories', {}),
                 AJAXService.ajaxWithToken('get', '/room/getRoomsAndStaus', {
-                    date: util.dateFormat(new Date()),
-                    days: 30,
+                    date: util.dateFormat(this.startDate),
+                    days: this.days,
                     sub: true,
                 }),
-            ])
-                .then(res => {
-                    console.log(res);
-                    const pRooms = res[0].data.list;
-                    const { holidays } = res[1].data
-                })
+            ]).then(res => {
+                    this.categories = res[0].data.list;
+                    const { holidays, orderList, rs } = res[1].data;
+                    this.holidays = holidays;
+                    this.orderList = orderList;
+                    this.roomStatus = rs;
+                    this.roomStatus.map(room => {
+                        const category = this.categories.find(category => category.cId === room.ti);
+                        if (!category.rooms) {
+                            category.rooms = [];
+                        }
+
+                        category.rooms.push(room);
+                    })
+                });
+        },
+        data() {
+            return {
+                categories: [],
+                holidays: [],
+                roomStatus: [],
+                startDate: new Date(),
+                days: 30,
+            }
+        },
+        computed: {
+            dateRange() {
+                const arr = [];
+                const startDate = this.startDate;
+                for (let i = 0; i < this.days; i ++) {
+                    const date = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate() + i);
+                    const holiday = this.holidays.find(d => d.date === util.dateFormat(date));
+                    const isToday = util.isSameDay(date, new Date());
+                    const isHoliday = holiday && holiday.type === 0;
+                    arr.push({
+                        date,
+                        isToday,
+                        isHoliday,
+                        dateStr: isHoliday ? holiday.holiday : (isToday ? '今天' : util.dateFormatWithoutYear(date)),
+                        weekday: util.getWeek(date),
+                    })
+                }
+
+                return arr;
+            }
         },
         methods: {
 
--- static/js/app/src/accommodation/components/Calendar.vue
+++ static/js/app/src/accommodation/components/Calendar.vue
@@ -1,15 +1,45 @@
 <template>
     <div>
-        <!--<div class="calendar-leftHeader">
-            <div class="category-item" ng-repeat="c in cRoomArray" ng-if="pRoomList[c.pId].selected">
+        <div class="calendar-leftHeader">
+            <div class="category-item" v-for="c in categories">
                 <div class="category-name">
-                    <span class="stext">{{c.name}}</span>
+                    <span class="stext">{{c.cName}}</span>
                 </div>
                 <div class="category-list">
-                    <div class="room-item" room="{{r.id}}" ng-repeat="r in c.rooms">{{r.sn}}</div>
+                    <div class="room-item" v-for="r in c.rooms">{{r.sn}}</div>
                 </div>
             </div>
-        </div>-->
+        </div>
+        <div class="sheader">
+            <div class="sheader-item" v-for="d in dateRange">
+                <div class="date-item"
+                     :class="{'today':d.isToday, 'weekend': d.weekday == '周五' || d.weekday == '周六'}"
+                >
+                    <div class="date">
+                        {{d.dateStr}}
+                        <div class="eluyun_rest_outer spriteImg isHoliday" v-if="d.isHoliday">
+                            <div class="eluyun_rest"></div>
+                        </div>
+                    </div>
+                    <div class="desc">{{d.weekday}}</div>
+                </div>
+                <div class="left">剩{{}}</div>
+            </div>
+        </div>
+        <div class="calendor-container">
+            <div class="entryList">
+                <div class="srow" v-for="room in roomStatus">
+                    <div v-for="status in room.st">
+                        <div class="eluyun_tick_outer spriteImg">
+                            <div class="eluyun_tick"></div>
+                        </div>
+                        <div class="date">{{status.date}}</div>
+                        <div class="price">￥{{status.p}}</div>
+                        <div class="name">{{room.sn}}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <style lang="sass">
@@ -18,5 +48,11 @@
 </style>
 <script>
     export default{
+        props: {
+            categories: Array,
+            dateRange: Array,
+            holidays: Array,
+            roomStatus: Array,
+        }
     }
 </script>
