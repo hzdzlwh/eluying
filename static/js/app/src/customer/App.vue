8c87755c28be5dc46215cc3cab87cd7b9004b81b
lcx <258546853@qq.com>
2017-03-21 11:43:24 +0800

    feat(customer): refactor left menu

--- static/js/app/src/customer/App.vue
+++ static/js/app/src/customer/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<div class="customer-container">
     <ul class="leftMenu">
         <li class="leftMenu-item">
             <router-link to="/vip">会员客户</router-link>
@@ -11,10 +11,40 @@
             <router-link to="/non-vip">非会员客户</router-link>
         </li>
     </ul>
-    <router-view></router-view>
+    <div class="view-container">
+        <router-view></router-view>
+    </div>
 </div>
 </template>
-<style>
+<style  lang="sass" rel="stylesheet/scss">
+    @import "~dd-common-css/src/variables";
+    .reports-container {
+        padding-top: 89px;
+        padding-left: 244px;
+        padding-bottom: 50px;
+        position: relative;
+        .text-right {
+            text-align: right;
+        }
+    }
+    .view-container {
+        position: relative;
+        width: 1000px;
+        margin: 0 auto;
+    }
+    h5 {
+        font-size: $font-size-h5;
+    }
+    h4 {
+        font-size: $font-size-h4;
+    }
+    i {
+        color: #999;
+        font-style: inherit;
+    }
+    .dd-dropdown-menu {
+        z-index: 1;
+    }
 </style>
 <script>
     export default{
