dc8a61250c2f93a2090870f0b9b578fc2d296822
lcx <258546853@qq.com>
2017-03-21 14:13:49 +0800

    feat(customer):

--- static/js/app/src/customer/views/vip/list.vue
+++ static/js/app/src/customer/views/vip/list.vue
@@ -5,7 +5,7 @@
             <button>新增会员</button>
             <button>导出明细</button>
         </div>
-        <dd-table></dd-table>
+        <dd-table :columns="col" :data-source="vips"></dd-table>
         <div>
             <span>共计</span>
             <dd-pagination></dd-pagination>
@@ -15,14 +15,76 @@
 <style>
 </style>
 <script>
+    import { DdTable, DdPagination } from 'dd-vue-component';
+    import http from '../../../common/AJAXService';
     export default{
         data() {
             return {
-               
+                vips: [],
+                count: 0,
+                pageNo: 1,
+                col: [
+                    {
+                        title: '姓名',
+                        dataIndex: 'name'
+                    },
+                    {
+                        title: '会员等级',
+                        dataIndex: 'levelName'
+                    },
+                    {
+                        title: '会员卡号',
+                        dataIndex: 'vipCardNum'
+                    },
+                    {
+                        title: '手机号',
+                        dataIndex: 'phone'
+                    },
+                    {
+                        title: '消费金额',
+                        dataIndex: 'entireTotalConsume'
+                    },
+                    {
+                        title: '累计金额',
+                        dataIndex: 'totalConsume'
+                    },
+                    {
+                        title: '证件号',
+                        render: (h, row) => <span>{row.idCardNum}</span>
+                    },
+                    {
+                        title: '加入日期',
+                        render: (h, row) => <span>{row.createTime.split(' ')[0]}</span>
+                    },
+                    {
+                        title: '操作',
+                        render: (h, row) => <span>查单</span>
+                    }
+                ]
             }
         },
+        created() {
+            this.getVips();
+        },
         methods: {
-        
+            getVips() {
+                http.get('/vipUser/getVipUserListPC', {
+                    pageNo: this.pageNo,
+                    searchPattern: this.searchPattern,
+                    sortColumn: this.sortColumn,
+                    sortType: this.sortType
+                })
+                    .then(res => {
+                        if (res.code === 1) {
+                            this.vips = res.data.vipUserList;
+                            this.count = res.data.vipUserListSize;
+                        }
+                    })
+            }
+        },
+        components: {
+            DdTable,
+            DdPagination
         }
     }
 </script>
