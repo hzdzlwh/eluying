36f2c0ee3e6dee16f5b4a778db69aa6cdfa75f67
lcx <258546853@qq.com>
2017-03-28 17:54:57 +0800

    feat(insurance)

--- static/js/app/src/accommodation/components/OrderDetailModal.vue
+++ static/js/app/src/accommodation/components/OrderDetailModal.vue
@@ -367,22 +367,7 @@
                 </div>
             </div>
         </div>
-        <div class="modal fade roomModals" role="dialog" id="insuranceDialog">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="roomModals-header">
-                        保单详情
-                        <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
-                    </div>
-                    <div style="padding: 20px 24px; max-height: 485px">
-                        <dd-table :bordered="true" :columns="columns" :dataSource="order.insuranceInfoList || []"></dd-table>
-                    </div>
-                    <div class="roomModals-footer">
-                        <span>共{{order.insuranceInfoList && order.insuranceInfoList.length}}条保单记录，保费¥{{order.insuranceTotalPremium}}，已付款¥{{order.insuranceTotalPremium}}</span>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Insurance :order="order" />
     </div>
 </template>
 <style lang="sass" type="text/css" rel="stylesheet/scss">
@@ -663,7 +648,7 @@
     import modal from 'modal';
     import types from '../store/types';
     import { mapActions, mapState } from 'vuex';
-    import { DdTable } from 'dd-vue-component';
+    import Insurance from './Insurance.vue';
     export default{
         props: {
             orderId: {
@@ -679,47 +664,6 @@
                 ID_CARD_TYPE,
                 FOOD_STATE,
                 ORDER_STATUS_ICON,
-                columns: [
-                    {
-                        title: '被保人姓名',
-                        dataIndex: 'insurantsName',
-                        width: 80
-                    },
-                    {
-                        title: '手机号',
-                        render: (h, row) => (<span>{row.holderMobile || row.insurantsMobile}</span>),
-                        width: 105
-                    },
-                    {
-                        title: '性别',
-                        render: (h, row) => (<span>{['','男','女'][row.insurantsSex]}</span>),
-                        width: 38
-                    },
-                    {
-                        title: '年龄',
-                        dataIndex: 'insurantsAge',
-                        width: 38
-                    },
-                    {
-                        title: '投保日期',
-                        dataIndex: 'startDate',
-                        width: 90
-                    },
-                    {
-                        title: '终保日期',
-                        dataIndex: 'endDate',
-                        width: 90
-                    },
-                    {
-                        title: '保单号',
-                        dataIndex: 'proposalNo'
-                    },
-                    {
-                        title: '创建时间',
-                        dataIndex: 'date',
-                        width: 155
-                    }
-                ]
             }
         },
         computed: {
@@ -889,7 +833,7 @@
             }
         },
         components:{
-            DdTable
+            Insurance
         },
         watch: {
             orderDetailShow(newVal, oldVal) {
