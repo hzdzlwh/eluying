498d073a4a99f4e8f260a383ea71b29224078c59
lcx <258546853@qq.com>
2017-03-28 19:07:34 +0800

    feat(order)

--- static/js/app/src/ordersManage/components/OrderContainer.vue
+++ static/js/app/src/ordersManage/components/OrderContainer.vue
@@ -4,13 +4,19 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="roomModals-header">
-                        <div class="header-container" v-if="order.orderState">
+                        <div class="header-container">
+                            <img v-if="order.orderType === ORDER_TYPE.ACCOMMODATION" src="/static/image/room-icon.png" alt="">
+                            <img v-if="order.orderType === ORDER_TYPE.ENTERTAINMENT" src="" alt="">
+                            <img v-if="order.orderType === ORDER_TYPE.CATERING" src="" alt="">
+                            <img v-if="order.orderType === ORDER_TYPE.RETAIL" src="/static/image/shop-icon.png" alt="">
                             <span class="header-text">{{title}}</span>
-                            <span class="order-state-angle"
-                                  :style="{ borderColor: ORDER_STATUS_ICON[order.orderState]['borderColor']}"></span>
-                            <span class="order-state"
-                                  :style="{ background: ORDER_STATUS_ICON[order.orderState]['backgroundColor']}"
-                                  v-text="ORDER_STATUS_ICON[order.orderState]['text']">
+                            <span v-if="order.orderState">
+                                <span class="order-state-angle"
+                                      :style="{ borderColor: ORDER_STATUS_ICON[order.orderState]['borderColor']}"></span>
+                                <span class="order-state"
+                                      :style="{ background: ORDER_STATUS_ICON[order.orderState]['backgroundColor']}"
+                                      v-text="ORDER_STATUS_ICON[order.orderState]['text']">
+                                </span>
                             </span>
                         </div>
                         <div class="header-container">
