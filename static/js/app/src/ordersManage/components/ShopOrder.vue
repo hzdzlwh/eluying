586d6702cd8372806e4f470d15dcc7b7f89d29c1
赵永生 <zhaoyongsheng@eluying.com>
2017-03-28 17:23:57 +0800

    fix: modify file name

---
+++ static/js/app/src/ordersManage/components/ShopOrder.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="content-item">
+            <p class="content-item-title"><span>商超信息</span></p>
+            <div class="items">
+                <div class="item">
+                    <div class="orderDetailModal-shop-item">
+                        <span class="shop-icon"></span>
+                        <div class="item-content">
+                            <span class="shop-time small-font">2017-03-28 14:33</span>
+                            <div style="margin-right: 81px">
+                                <label class="label-text">小计</label>
+                                <span>¥400</span>
+                            </div>
+                            <span class="discount-info" style="top: 14px">
+                                                <span>原价<span class="origin-price">¥500</span></span>
+                                                <span class="discount-num">
+                                                    会员3折
+                                                </span>
+                                            </span>
+                        </div>
+                    </div>
+                    <div class="item-content">
+                        <span style="padding-left: 32px; width: 120px;">零食一号</span>
+                        <span>x4</span>
+                        <span style="margin-right: 304px;width: 120px; text-align: right">¥200</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" type="text/css" rel="stylesheet/scss">
+</style>
+<script>
+    export default{
+        data() {
+            return {
+               
+            };
+        },
+        methods: {
+        
+        }
+    };
+</script>
