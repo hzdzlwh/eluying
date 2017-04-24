b5af8aa919ece376b1715d1214551cfeb9a731c4
lingchenxuan <lingchenxuan@eluying.com>
2017-04-24 14:23:16 +0800

    fix: fixed #4155

--- static/js/app/src/accommodation/App.vue
+++ static/js/app/src/accommodation/App.vue
@@ -85,6 +85,7 @@
     import http from 'http';
     import { mapMutations } from 'vuex';
     import types from '../ordersManage/store/types';
+
     export default{
         created() {
             bus.$on('onClose', this.hideDetail);
