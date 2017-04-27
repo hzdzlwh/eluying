d354228a01e82fd46559aadb1fd5bed3b522c7f9
赵永生 <zhaoyongsheng@eluying.com>
2017-04-27 18:06:27 +0800

    fix(2.6.1): fixed deposit bug

--- static/js/app/src/ordersManage/components/CashierModal.vue
+++ static/js/app/src/ordersManage/components/CashierModal.vue
@@ -55,7 +55,7 @@
                             <div class="cashier-deposit-container">
                                 <div class="cashier-deposit-info" v-if="showDeposit">
                                     <span>押金:</span>
-                                    <input type="number" class="dd-input" v-model="deposit" placeholder="请输入押金金额">
+                                    <input type="number" class="dd-input" v-model.number="deposit" placeholder="请输入押金金额">
                                     <span style="margin-left: 24px">{{(orderPayment.deposit || 0) - (orderPayment.refundDeposit || 0) > 0 && type !== 'checkIn' ? '退款' : '收款'}}方式:</span>
                                     <dd-select v-model="depositPayChannel" :placeholder="`请选择${(orderPayment.deposit || 0) - (orderPayment.refundDeposit || 0) > 0 && type !== 'checkIn' ? '退款' : '收款'}方式`">
                                         <dd-option v-for="payChannel in depositPayChannels" :key="payChannel.channelId" :value="payChannel.channelId" :label="payChannel.name">
