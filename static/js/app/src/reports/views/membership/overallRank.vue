ec2612b869aaa7fdcd260482e0cced42eedd2254
info <info3781@gmail.com>
2017-05-19 13:20:37 +0800

    add:会员卡报表导出功能

--- static/js/app/src/reports/views/membership/overallRank.vue
+++ static/js/app/src/reports/views/membership/overallRank.vue
@@ -7,7 +7,7 @@
             <div class="search">
 	            <input type="text" class="dd-input" placeholder="搜索姓名/手机号/证件号/会员卡号" @keyup.enter="search" ref="searchInput">
 	            <img class="search-icon" @click="search" src="//static.dingdandao.com/vipSearch.png">
-                <a :href="exportUrl"><button class="dd-btn dd-btn-primary">导出Excel</button></a>
+                <a :href="exportUrl" download><button class="dd-btn dd-btn-primary">导出Excel</button></a>
 	        </div>
         </div>
         <div>
@@ -83,6 +83,19 @@
         computed: {
             ...mapState(['date']),
             exportUrl() {
+                const paramsObj = {
+                    exportType: 0,
+                    reportType: 14,
+                    params: JSON.stringify({
+                        startDate: this.date.startDate,
+                        endDate: this.date.endDate
+                    })
+                };
+                const host = http.getUrl('/stat/exportReport');
+                const pa = http.getDataWithToken(paramsObj);
+                pa.params = JSON.parse(pa.params);
+                const params = http.paramsToString(pa);
+                return `${host}?${params}`;
             }
         },
         created() {
