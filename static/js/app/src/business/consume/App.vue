5a24eccab3b87b44599a3d418901c0771ba5f964
info <info3781@gmail.com>
2017-05-27 18:39:28 +0800

    add:其他消费页面

---
+++ static/js/app/src/business/consume/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <outer-container style="width:600px; float:left;" :title='consumeItem' :add="addItem">
+        </outer-container>
+        <outer-container style="width:200px; position:fixed; margin-left:620px;" :title='classifyManage' :add="addClassify"></outer-container>
+    </div>
+</template>
+
+<script>
+    import outerContainer from './components/outerContainer';
+
+    export default {
+        data() {
+            return {
+                consumeItem: '消费项目',
+                addItem: '新增项目',
+                classifyManage: '分类管理',
+                addClassify: '新增分类'
+            }
+        },
+        components: {
+            outerContainer
+        }
+    }
+</script>
+
+<style>
+    
+</style>
---
+++ static/js/app/src/business/consume/components/outerContainer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="outer-container">
+        <header class="outer-title">
+            <h4>{{title}}</h4>
+            <a><span>+</span>{{add}}</a>
+        </header>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: '',
+            add: '',
+        }
+    }
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+    .outer-container{
+        border-radius: 2px;
+        border-top: 4px solid #178ce6;
+        box-shadow: 0 2px 4px 0 rgba(0,0,0,0.15);
+        .outer-title{
+            display: flex;
+            justify-content: space-between;
+            padding: 10px;
+            background: #ddd;
+            border-bottom: 1px solid #ccc;
+        }
+    }
+</style>
