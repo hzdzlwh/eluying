368d78f0664ffcc888a65ee34b345f5e4945afad
lxj <xinjian.lin@dingdandao.com>
2017-06-16 16:34:42 +0800

    feat(2.9.0)

--- static/js/app/src/accommodation/components/dayOrder.vue
+++ static/js/app/src/accommodation/components/dayOrder.vue
@@ -10,13 +10,14 @@
                     <div class="taday-status-title">{{item.zoneName}}</div>
                     <div class="taday-status-content">
                         <div class="taday-status-item" v-for='(it, itemIndex) in item.roomList' @contextmenu.prevent="$refs.ctxMenu.open($event, {data: it})" @click='setSelect(it, contentIndex, itemIndex)' :style="{background:colorList[it.roomState]}" @mouseenter="hoverShow($event, it)" @mouseleave="it.hover = false">
-                        <div class="taday-status-mark" v-if='it.roomState === 1 || it.roomState === 2 ||it.roomState === 3 '>
-                            {{it.roomState === 1 ? '保留': it.roomState === 2 ? '维修' : '停用'}}
-                        </div>    
+                            <div class="taday-status-mark" v-if='it.roomState === 1 || it.roomState === 2 ||it.roomState === 3 '>
+                                {{it.roomState === 1 ? '保留': it.roomState === 2 ? '维修' : '停用'}}
+                            </div>
                             <hover :date='it' :hoverShow='hoverEvent' class='calendar-glyph-hover' v-if='it.checkInDate'></hover>
                             <div class="taday-status-item-select" v-if='it.isSelect'></div>
                             <div class="taday-status-item-title" :title='it.roomName'>
-                                {{it.roomName}}
+                                <div class="taday-status-item-title2">{{it.roomName}}</div>
+                                <div class="taday-status-item-title3">{{it.roomName}}</div>
                             </div>
                             <div class="taday-status-item-name" :title='it.customerName'>{{it.customerName}}</div>
                             <div style='display:inline-block'>
@@ -104,13 +105,15 @@
         }
     }
 }
-.taday-status-mark{
+
+.taday-status-mark {
     position: absolute;
     top: 40px;
     left: 60px;
     opacity: 0.2;
     font-size: 30px;
 }
+
 .taday-calendar-picker {
     position: relative;
     top: 48px;
@@ -128,7 +131,7 @@
     .taday-status-item {
         padding: 8px;
         border-radius: 4px;
-        width: 124px;
+        width: 130px;
         height: 82px;
         color: #ffffff;
         display: inline-block;
@@ -140,8 +143,9 @@
                 position: absolute;
             }
         }
-        .taday-status-item-title {
-            display: block;
+        .taday-status-item-title2 {
+            display: inline-block;
+            width: 70px;
             white-space: nowrap;
             overflow: hidden;
             text-overflow: ellipsis;
@@ -149,6 +153,18 @@
             line-height: 20px;
             text-align: left;
             font-weight: bold;
+            float: left;
+        }
+        .taday-status-item-title3 {
+            display: inline-block;
+            font-size: 14px;
+            line-height: 20px;
+            text-align: left;
+            font-weight: bold;
+            width: 40px;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
         }
         .taday-status-item-name {
             width: 100%;
@@ -181,8 +197,8 @@
 .taday-status-item-select {
     background: url(/static/image/modal/roomSelect.png);
     background-color: rgba(255, 255, 255, 0.4);
-    width:100%;
-    height:100%;
+    width: 100%;
+    height: 100%;
     margin: -8px;
     position: absolute;
     background-repeat: no-repeat;
