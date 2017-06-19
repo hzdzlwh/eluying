2f9b83086bc344ddb762f5b3e5ae4741885a0bb3
lwh <wenhua.li@dingdandao.com>
2017-06-19 19:41:48 +0800

    add:房态表

---
+++ static/js/app/src/accommodation/components/dayOrderLeft.vue
@@ -0,0 +1,30 @@
+<template>
+    <div :class="{ movedown: $route.path !== '/nowOrders/houseMap'}">
+        <slot name="timePicker"></slot>
+        <ul>
+            <li>
+                <router-link to="/nowOrders/houseMap">房态图</router-link>
+                <slot name="filterCondition"></slot>
+            </li>
+            <li>
+                <router-link to="/nowOrders/preManage">预定管理</router-link>
+            </li>
+            <li>
+                <router-link to="/nowOrders/houseTable">房态表</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+    };
+</script>
+
+<style scoped>
+    .movedown{
+        position: relative;
+        top: 48px;
+    }
+</style>
+
