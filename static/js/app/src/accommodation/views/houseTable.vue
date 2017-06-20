80323ab5cafdac21c4f83e7c3d9022939264a030
lwh <wenhua.li@dingdandao.com>
2017-06-20 18:08:48 +0800

    add: 房态表

--- static/js/app/src/accommodation/views/houseTable.vue
+++ static/js/app/src/accommodation/views/houseTable.vue
@@ -1,8 +1,63 @@
 <template>
-    <div>
+    <div style="display: flex;">
         <dayOrderLeft>
             <DateSelect slot="timePicker" :defaultDate="defaultStartDate" @change='changeDate' :width='185' :disabledDate='true' />
         </dayOrderLeft>
+        <div class="content">
+            <div class="table-container">
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>可售房<div class="info-icon"></div></th>
+                            <th>可用房<div class="info-icon"></div></th>
+                            <th>在住</th>
+                            <th>预离</th>
+                            <th>预抵</th>
+                            <th>保留房</th>
+                            <th>维修房</th>
+                            <th>停用房</th>
+                            <th>总房数</th>
+                            <th>净房</th>
+                            <th>脏房</th>
+                            <th>出租率<div class="info-icon"></div></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>标准间</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>100%</td>
+                        </tr>
+                        <tr>
+                            <td>大床房</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>3</td>
+                            <td>100%</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -26,6 +81,41 @@
     };
 </script>
 
-<style>
-    
+<style lang="scss" rel="stylesheet/scss" scoped>
+    .content{
+        flex: 1;
+        padding-top: 50px;
+        .table-container{
+            width: 1073px;
+            padding: 30px 0 0 33px;
+            table{
+                width: 100%;
+                border: 1px solid #e6e6e6;
+                border-top: 8px solid #178ce6;
+                thead{
+                    background: #f1f8ff;
+                }
+                tbody{
+                    tr:nth-of-type(even){
+                        background: #f7f7f7;
+                    }
+                }
+                tr{
+                    height: 46px;
+                    th{
+                        font-size: 12px;
+                        color: #666666;
+                        font-weight: bold;
+                        text-align: center;
+                    }
+                    td{
+                        font-size: 14px;
+                        color: #666666;
+                        text-align: center;
+                    }
+
+                }
+            }
+        }
+    }
 </style>
