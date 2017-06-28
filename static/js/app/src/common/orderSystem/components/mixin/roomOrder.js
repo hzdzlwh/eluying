16abce7b2d25a9e53df5c670f0fa6d436eafc5e9
lxj <xinjian.lin@dingdandao.com>
2017-06-28 09:39:07 +0800

    feat(2.10.1)

--- static/js/app/src/common/orderSystem/components/mixin/roomOrder.js
+++ static/js/app/src/common/orderSystem/components/mixin/roomOrder.js
@@ -12,24 +12,26 @@ export default {
         disabledStartDate(endDate) {
             const str = util.dateFormat(new Date(endDate));
             const arr = str.split('-');
-            if (this.checkState === 'finish') {
+            if (this.checkState === 'finish' || this.checkState === 'quick' || this.checkState === 'team') {
                 return (date) => {
                     return date.valueOf() >= (new Date(arr[0], arr[1] - 1, arr[2])).valueOf();
                 };
-            } else if (this.checkState === 'ing' || this.checkState === 'ing') {
+            }
+            if (this.checkState === 'ing' || this.checkState === 'checkIn') {
                 return (date) => {
-                    return date.valueOf() <=  (new Date(arr[0], arr[1] - 1, arr[2])).valueOf();
-                };
-            } else {
-                return (date) => {
-                    return date.valueOf() < (new Date(arr[0], arr[1] - 1, arr[2])).valueOf();
+                    return date.valueOf() <= (new Date(arr[0], arr[1] - 1, arr[2])).valueOf();
                 };
             }
+            if (this.checkState === 'book' || this.checkState === 'editOrder') {
+                return (date) => {
+                    return date.valueOf() >= (new Date(arr[0], arr[1] - 1, arr[2])).valueOf();
+                };
+            }
+            return (date) => {
+                return date.valueOf() < (new Date(arr[0], arr[1] - 1, arr[2])).valueOf();
+            };
         },
         disabledEndDate(startDate, type) {
-            if (type) {
-                return true;
-            }
             if (this.checkState === 'quick' || this.checkState === 'team') {
                 const str = util.dateFormat(new Date(startDate));
                 const arr = str.split('-');
