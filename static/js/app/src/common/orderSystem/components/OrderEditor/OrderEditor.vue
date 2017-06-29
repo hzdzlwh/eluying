9a60f0064bdcee284c661a2caa5922611b88ace2
lwh <wenhua.li@dingdandao.com>
2017-06-29 15:28:48 +0800

    fix:#1061

--- static/js/app/src/common/orderSystem/components/OrderEditor/OrderEditor.vue
+++ static/js/app/src/common/orderSystem/components/OrderEditor/OrderEditor.vue
@@ -160,7 +160,7 @@
                     </div>
                     <div class="roomModals-footer">
                         <div>
-                         <div @click="returnPreStep" v-if="hasBack || checkState === 'checkIn'" class="btn-back" style='    display: inline-block;'><img src="/static/image/modal/back.png" alt=""></div>
+                         <div @click="returnPreStep" v-if="(hasBack || checkState === 'checkIn') && $route.path !== '/nowOrders/preManage'" class="btn-back" style='    display: inline-block;'><img src="/static/image/modal/back.png" alt=""></div>
                             <span class="footer-label">订单金额</span>
                             <span class="footer-price">¥{{totalPrice}}</span>
                         </div>
