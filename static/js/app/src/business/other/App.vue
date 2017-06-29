31cd625464762415db7bc256a95072331d75b4f0
lwh <wenhua.li@dingdandao.com>
2017-06-29 17:29:03 +0800

    feat:其他设置

---
+++ static/js/app/src/business/other/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="app">
+        <outer-container :title="disconuntAndChange">
+            <inner-container :title="shortcutDiscount"></inner-container>
+            <inner-container :title="changeProcess" style="margin-top: 18px;"></inner-container>
+        </outer-container>
+        <outer-container :title="statisticParameter" style="margin-top: 20px;">
+            <inner-container></inner-container>
+            <inner-container></inner-container>
+        </outer-container>
+    </div>
+</template>
+
+<script>
+    import outerContainer from './components/outerContainer';
+    import innerContainer from './components/innerContainer';
+
+    export default {
+        data() {
+            return {
+                disconuntAndChange: '折扣及零头处理',
+                statisticParameter: '统计参数',
+                shortcutDiscount: '快捷折扣',
+                changeProcess: '零头处理'
+            }
+        },
+        components: {
+            outerContainer,
+            innerContainer
+        }
+    }
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+    
+</style>
