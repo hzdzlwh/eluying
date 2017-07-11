2bf3fb5f4a1190c2f937b7f64e1a73990b565e83
lxj <xinjian.lin@dingdandao.com>
2017-07-11 14:53:55 +0800

    fix #1249

--- static/js/app/src/common/orderSystem/components/Detail/OrderContainer.vue
+++ static/js/app/src/common/orderSystem/components/Detail/OrderContainer.vue
@@ -197,7 +197,7 @@
                         <div style="width: 100%;">
                             <div class="order-btns">
                                 <span v-if="this.order.roomInfo || this.order.rooms && this.order.rooms.length > 0">
-                                    <div class="dd-btn dd-btn-primary order-btn" v-if="((order.type !== ORDER_TYPE.COMBINATION && order.isCombinationOrder) || (order.type === ORDER_TYPE.ACCOMMODATION && !order.isCombinationOrder)) && getRoomsState.transform" @click='editOrder("transform")'
+                                    <div class="dd-btn dd-btn-primary order-btn" v-if="getRoomsState.transform" @click='editOrder("transform")'
                                         >
                                         转正常入住
                                     </div>
