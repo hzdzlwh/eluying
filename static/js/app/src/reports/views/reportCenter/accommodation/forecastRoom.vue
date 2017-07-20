84094425788f98310d0bcb273bc30abaa875e610
sunxiaojun <xiaojun.sun@dingdandao.com>
2017-07-20 15:13:58 +0800

    'report-reportCenter-forecastRoom'

--- static/js/app/src/reports/views/reportCenter/accommodation/forecastRoom.vue
+++ static/js/app/src/reports/views/reportCenter/accommodation/forecastRoom.vue
@@ -5,14 +5,118 @@
             <span style="color:#999;font-size:14px;">～</span>
             <DdDatepicker placeholder="结束时间" v-model="endTime" :disabled-date="disableEndDate" />
         </div>
+        <div class="report-forecastRoom-title">
+            {{$route.meta.name}}
+        </div>
+        <div class="report-forecastRoom-date">
+            {{startTime}} ~ {{endTime}}
+        </div>
+        <table class="report-forecastRoom-table" border="1">
+            <thead>
+                <tr>
+                    <th rowspan="2">营业日期</th>
+                    <th colspan="3">在住</th>
+                    <th colspan="3">预抵</th>
+                    <th colspan="2">预留</th>
+                    <th rowspan="2">停用维修</th>
+                    <th rowspan="2">房间存量</th>
+                </tr>
+                <tr>
+                    <th>房间</th>
+                    <th>客人</th>
+                    <th>当日房费</th>
+                    <th>房间</th>
+                    <th>客人</th>
+                    <th>当日房费</th>
+                    <th>房间</th>
+                    <th>客人</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in vips">
+                    <td>{{item.date}}</td>
+                    <td>{{item.usingRoom}}</td>
+                    <td>{{item.usingResident}}</td>
+                    <td>{{item.usingPrice}}</td>
+                    <td>{{item.preOrderedRoom}}</td>
+                    <td>{{item.preOrderedResident}}</td>
+                    <td>{{item.preOrderedPrice}}</td>
+                    <td>{{item.leavingRoom}}</td>
+                    <td>{{item.leavingResident}}</td>
+                    <td>{{item.repairingRoom}}</td>
+                    <td>{{item.stock}}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
-<style>
-    
+<style lang="scss" scoped>
+    .report-forecastRoom-title{
+        font-family:MicrosoftYaHei-Bold;
+        font-size:24px;
+        color:#178ce6;
+        text-align:center;
+    }
+    .report-forecastRoom-table{
+        width: 100%;
+        font-size: 12px;
+        border: 1px solid #CCCCCC;
+        thead{
+            tr{
+                background: #99CCFF;
+                font-size: 12px;
+                line-height: 28px;
+                font-family:MicrosoftYaHei;
+                color:#666666;
+                th{
+                    text-align: center;
+                    padding: 0 10px;
+                }
+            }
+        }
+        tbody{
+            tr{
+                td:first-child{
+                    padding-left: 34px;
+                }
+                td:not(:first-child){
+                    text-align: right;
+                    padding-right: 20px;
+                }
+            }
+            .mark-tr{
+                font-weight: bold;
+                color: #666666;
+                background: #b2dbff;
+                td:first-child{
+                    padding-left: 20px;
+                }
+            }
+            .word-bold{
+                font-weight: bold;
+                color: #666666;
+                padding-left: 34px;
+            }
+        }
+        tfoot{
+            tr{
+               background: #61758c;
+                color: #fff;
+                td:first-child{
+                    padding-left: 20px;
+                }
+                td:not(:first-child){
+                    text-align: right;
+                    padding-right: 20px;
+                }
+            }
+        }
+    }
 </style>
 <script>
     import { DdDatepicker } from 'dd-vue-component';
     import http from 'http';
+    import util from 'util';
     export default {
         data() {
             return {
@@ -29,7 +133,7 @@
         components: {
             DdDatepicker
         },
-        method: {
+        watch: {
             endTime() {
                 this.pageNo = 1;
                 if (this.flag) {
@@ -41,6 +145,24 @@
                 if (this.flag) {
                     this.fetchDate();
                 }
+            }
+        },
+        created() {
+            const startTime = new Date();
+            this.startTime = util.dateFormat(startTime);
+            this.endTime = util.dateFormat(util.diffDate(startTime, 30));
+            console.log(this.startTime, this.endTime);
+            this.getData();
+        },
+        methods: {
+            disableStartDate(date) {
+                if (this.startDate !== '') {
+                    const arr1 = this.startTime.split('-');
+                    const arr2 = this.endTime.split('-');
+                    return date.valueOf() < (new Date(arr1[0], arr1[1] - 1, arr1[2])).valueOf() || date.valueOf() > (new Date(arr2[0], arr2[1] - 1, arr2[2]));
+                } else {
+                    return true;
+                }
             },
             disableEndDate(date) {
                 if (this.startTime !== '') {
@@ -50,19 +172,11 @@
                     return false;
                 }
             },
-            disableStartDate(date) {
-                if (this.endDate !== '') {
-                    const arr = this.endTime.split('-');
-                    return date && date.valueOf() > (new Date(arr[0], arr[1] - 1, arr[2])).valueOf();
-                } else {
-                    return false;
-                }
-            },
-            getDate() {
-                http.get('/stat/getPredictionStat', { startDate: this.startTime, endDate: this.endTime }).then((res) => {
+            getData() {
+                http.get('/stat/getPredictionStat', { startDate: this.startTime, toDate: this.endTime }).then((res) => {
                     if (res.code === 1) {
-                        console.log(res);
-                    }
+                        this.vips = res.data.entityList;
+                    };
                 });
             }
         }
