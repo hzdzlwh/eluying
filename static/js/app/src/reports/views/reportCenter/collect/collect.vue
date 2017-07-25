6237623905c98eab74a9e6300e8754eff7da4006
lxj <xinjian.lin@dingdandao.com>
2017-07-25 12:33:13 +0800

    feat(2.11): 订单重点

--- static/js/app/src/reports/views/reportCenter/collect/collect.vue
+++ static/js/app/src/reports/views/reportCenter/collect/collect.vue
@@ -1,17 +1,28 @@
 <template>
     <div>
-        <AsyncComp></AsyncComp>
+        <accommodation v-if='componentName === "301"'></accommodation>
     </div>
 </template>
 <style lang="scss" scoped>
 </style>
 <script>
-    // import collectLinkList from '../collectList.js';
+    import accommodation from '../accommodation/morrowArrive.vue';
+    import collectList from '../../../collectList.js';
     // const hrefArr = location.pathname.split('/');
     export default {
         data() {
             return {
+                collectList,
+                componentName: this.$route.params.id
             };
-        }
+        },
+        components: {
+            accommodation
+            // 'accommodation': routeList.accommodation
+        },
+        // beforeRouteUpdate(to, from, next) {
+        //     this.componentName = this.$routes.params.id;
+        //     next();
+        // }
     };
 </script>
