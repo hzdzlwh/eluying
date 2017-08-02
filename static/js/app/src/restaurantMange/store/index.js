3fce236c9b372cbfd88a38fff4712725cd172764
lwh <wenhua.li@dingdandao.com>
2017-08-02 15:36:41 +0800

    feat:餐饮

--- static/js/app/src/restaurantMange/store/index.js
+++ static/js/app/src/restaurantMange/store/index.js
@@ -1,8 +1,8 @@
 /*
 * @Author: lxj
 * @Date:   2017-07-31 10:52:58
-* @Last Modified by:   linxinjian
-* @Last Modified time: 2017-07-31 10:57:48
+* @Last Modified by:   Tplant
+* @Last Modified time: 2017-08-02 15:07:50
 * @email: 783384903@qq.com
 */
 
@@ -10,15 +10,20 @@
 import types from './types';
 import Vuex from 'vuex';
 import Vue from 'vue';
+import { dateFormat } from '../../common/util';
 
 Vue.use(Vuex);
 const store = new Vuex.Store({
     state: {
-        restId: 0
+        restId: 0,
+        date: dateFormat(new Date())
     },
     mutations: {
         [types.SET_REST](state, { restId }) {
             state.restId = restId;
+        },
+        [types.SET_DATE](state, { date }) {
+            state.date = date;
         }
     },
     actions: {
